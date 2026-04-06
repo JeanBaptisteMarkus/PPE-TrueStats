ba083585f5e1fe537c9d7fce00b379da0ac42c84
bagard <guillaume.bgrd@icloud.com>
2026-04-06 14:48:38 +0200

    changement couleur drawer des joueurs

--- basketball_analysis/drawers/player_tracks_drawer.py
+++ basketball_analysis/drawers/player_tracks_drawer.py
@@ -3,7 +3,7 @@ import cv2
 
 class PlayerTracksDrawer:
 
-    def __init__(self, team_1_color=(255,245,238), team_2_color=(128,0,0)):
+    def __init__(self, team_1_color=(255,255,255), team_2_color=(0,230,0)):
         self.default_player_team_id = 1
         self.team_1_color = team_1_color
         self.team_2_color = team_2_color
